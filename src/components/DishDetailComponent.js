d86e66248430f82083c2045d17b7a14e9df949df
Dakshin S.A <dakshinsa2107@gmail.com>
2020-11-03 09:56:46 +0530

    week 3 completed correctly

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import { Media } from 'reactstrap';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import CommentForm from './CommentFormComponent.js';
+import { Control, LocalForm, Errors } from 'react-redux-form';
+import {Modal, ModalBody, ModalHeader, Button, Col, Row, Label } from 'reactstrap';
+
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
 
 	function RenderComments({dishes_comments}){
     	if (dishes_comments != null){
@@ -72,5 +76,96 @@ import CommentForm from './CommentFormComponent.js';
 	}
 
 
+class CommentForm extends Component{
+
+	constructor(props){
+		super(props);
+		this.state = {
+			isCommentModalOpen : false
+		};
+		this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
+		this.toggleCommentModal = this.toggleCommentModal.bind(this);
+
+	}
+
+	handleCommentSubmit(values){
+		console.log('Current State is: ' + JSON.stringify(values));
+		alert('Current State is: ' + JSON.stringify(values));
+	}
+
+	toggleCommentModal(){
+		this.setState({
+			isCommentModalOpen: !(this.state.isCommentModalOpen)
+		});
+	}
+
+	render(){
+		return(
+			<div className='container'>
+				<Button outline onClick={this.toggleCommentModal}>
+					<span className="fa fa-pencil fa-lg"></span>Submit Comment
+				</Button>
+				<Modal isOpen={this.state.isCommentModalOpen} toggle={this.toggleCommentModal}>
+					<ModalHeader toggle={this.toggleCommentModal}>Submit Comment</ModalHeader>
+					<ModalBody>
+						<LocalForm onSubmit={(values) => this.handleCommentSubmit(values)}>
+							<Row className="form-group">
+								<Label htmlFor='Rating' md={12}>Rating</Label>
+								<Col md={12}>
+									<Control.select model=".contactType" name="contactType"
+										className="form-control">
+										<option>1</option>
+										<option>2</option>
+										<option>3</option>
+										<option>4</option>
+										<option>5</option>
+									</Control.select>
+								</Col>
+							</Row>
+							<Row className="form-group">
+								<Label htmlFor="firstname" md={6}>Your Name</Label>
+								<Col md={12}>
+									<Control.text model=".name" id="name" name="name"
+										placeholder="Name"
+										className="form-control"
+										validators={{
+											minLength: minLength(3), maxLength: maxLength(15)
+										}}
+										/>
+									<Errors
+										className="text-danger"
+										model=".name"
+										show="touched"
+										messages={{
+											minLength: 'Must be greater than 2 characters',
+											maxLength: 'Must be 15 characters or less'
+										}}
+									/>
+								</Col>
+							</Row>
+							<Row className="form-group">
+								<Label htmlFor="message" md={6}>Comment</Label>
+								<Col md={12}>
+									<Control.textarea model=".comment" id="comment" name="comment"
+										rows="6"
+										className="form-control" />
+								</Col>
+							</Row>
+							<Row className="form-group">
+								<Col md={5}>
+									<Button type="submit" color="primary">
+									Submit
+									</Button>
+								</Col>
+							</Row>
+						</LocalForm>
+					</ModalBody>
+				</Modal>
+			</div>
+		);
+
+	}
+}
+	
 
 export default DishDetail;
